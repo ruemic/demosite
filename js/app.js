86e4917b156b638bb2a32ff19959937aa70e95da
ruemic <rumicdesign@gmail.com>
2013-08-22 07:33:36 -0700

    first

---
+++ js/app.js
@@ -0,0 +1,20 @@
+;(function ($, window, undefined) {
+  'use strict';
+
+  var $doc = $(document),
+      $win = $(window),
+      Modernizr = window.Modernizr;
+  
+  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
+  if (Modernizr.touch && !window.location.hash) {
+    $win.load(function() {
+      setTimeout(function() {
+        window.scrollTo(0, 1);
+      }, 0);
+    });
+  }
+  
+
+
+
+})(jQuery, this);
